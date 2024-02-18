86e00de78c64cd4b98fb2d9c75bf5d74e518f34f
Bryan Yep <yepsan95@gmail.com>
2024-02-18 13:24:48 -0800

    fix: typo

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const process = require('process');
-const filePath = process.argv[1];
+const filePath = process.argv[2];
 fs = require('fs');
 
-fs.readFile(filePath, 'utf8' (err, data) => {
+fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
