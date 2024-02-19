01e5c1702c239fbf127e2a7bbaa15f6599cd152b
Bryan Yep <yepsan95@gmail.com>
2024-02-18 20:47:49 -0800

    fix: function sleeps for 1 second.

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -2,31 +2,19 @@
 const request = require('request');
 const movieId = process.argv[2];
 const apiURL = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
-async function getCharacterName(characterURL) {
-  request(characterURL, function (err, response, body) {
-    if (err) {
-      console.error(err);
-    }
-    const character = JSON.parse(body);
-	return new Promise(resolve => {
-      setTimeout(() => {
-      console.log(character.name);
-      resolve();
-	  }, 100);
-    });
-  });
-}
-
-async function getFilmData(apiURL) {
-  request(apiURL, function (err, response, body) {
-    if (err) {
-      console.error(err);
-    }
-    const film = JSON.parse(body);
-    for (const characterURL of film.characters) {
-      await getCharacterName(characterURL);
-    }
+request(apiURL, function (err, response, body) {
+  if (err) {
+    console.error(err);
   }
-}
-
-getFilmData(apiURL);
+  const film = JSON.parse(body);
+  for (const characterURL of film.characters) {
+    request(characterURL, function (err, response, body) {
+      if (err) {
+        console.error(err);
+      }
+	  setTimeout(1000);
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  }
+});
