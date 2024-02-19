44d461de03f64032401e4a9dea3df92693728132
Bryan Yep <yepsan95@gmail.com>
2024-02-19 09:13:47 -0800

    fix: data from the API is now fetched synchronously using recursion.

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -2,18 +2,31 @@
 const request = require('request');
 const movieId = process.argv[2];
 const apiURL = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
 request(apiURL, function (err, response, body) {
   if (err) {
     console.error(err);
+    return;
   }
   const film = JSON.parse(body);
-  for (const characterURL of film.characters) {
-    request(characterURL, function (err, response, body) {
+  const characters = film.characters;
+
+  function fetchCharacter (index) {
+    if (index >= characters.length) {
+      return;
+    }
+
+    request(characters[index], function (err, response, body) {
       if (err) {
         console.error(err);
+        return;
       }
+
       const character = JSON.parse(body);
       console.log(character.name);
+      fetchCharacter(index + 1);
     });
   }
+
+  fetchCharacter(0);
 });
