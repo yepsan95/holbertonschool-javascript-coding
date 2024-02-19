69395269e0711f9862504454ba0f80c862eb5f1d
Bryan Yep <yepsan95@gmail.com>
2024-02-18 17:07:04 -0800

    fix: improved code

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
 const apiURL = process.argv[2];
+const characterId = 18;
 request(apiURL, function (err, response, body) {
   if (err) {
     console.error(err);
   }
   const jsonString = body;
-  const jsonObject = JSON.parse(jsonString);
+  const films = JSON.parse(jsonString).results;
   let wedgeAntillesCount = 0;
-  const wedgeURL = 'https://swapi-api.hbtn.io/api/people/18/';
-  for (const movie of jsonObject.results) {
-    if (movie.characters.includes(wedgeURL)) {
-      wedgeAntillesCount++;
+    for (const film of films) {
+      for (const character in film.characters) {
+        if (character.includes(`${characterId}`)) {
+          wedgeAntillesCount++;
+        }
+      }
     }
-  }
   console.log(wedgeAntillesCount);
 });
