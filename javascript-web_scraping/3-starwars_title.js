b47e69fa9afe38f676f432412b901d3fb9f2a686
Bryan Yep <yepsan95@gmail.com>
2024-02-18 16:33:05 -0800

    fix: semistandard compliant

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,11 +2,11 @@
 const request = require('request');
 const movieId = process.argv[2];
 const apiURL = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
-request(apiURL, function(err, response, body) {
+request(apiURL, function (err, response, body) {
   if (err) {
     console.error(err);
   }
-  let jsonString = body;
-  let jsonObject = JSON.parse(jsonString);
+  const jsonString = body;
+  const jsonObject = JSON.parse(jsonString);
   console.log(jsonObject.title);
 });
