4bbe79e123d87a6c640f056bce03e9512330e526
Bryan Yep <yepsan95@gmail.com>
2024-03-03 21:35:10 -0500

    fix: ESLint compliant.

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,13 +2,13 @@ process.stdin.setEncoding('utf8');
 
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('readable', function() {
+process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name) {
     process.stdout.write(`Your name is: ${name}`);
   }
 });
 
-process.stdin.on('end', function() {
+process.stdin.on('end', () => {
   console.log('This important software is now closing');
 });
