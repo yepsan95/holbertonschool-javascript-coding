0db1089965b8a1f4bcc9d932d12684b4e1f2f8d1
Bryan Yep <yepsan95@gmail.com>
2024-03-03 21:30:30 -0500

    fix: changed code structure

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,8 +2,9 @@ process.stdin.setEncoding('utf8');
 
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', function(data) {
-  process.stdout.write('Your name is: ' + data);
+process.stdin.on('readable', function() {
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is: ${name}`);
 });
 
 process.stdin.on('end', function() {
